03a6f04a95cfa07f925293224cf6735f1d0f13ce
ImaCrea <maximeguedj@pm.me>
2024-03-31 00:44:31 +0100

    update link

--- public/assets/javascripts/hello.js
+++ public/assets/javascripts/hello.js
@@ -1,5 +1,5 @@
 $(document).ready(
 	function() {
-        console.log("Hello Geeky Friend, want to help us improve %cM%cA%cI%cL%cT%cA%cP%cE%c experience? > contact@%cm%ca%ci%cl%ct%ca%c.%cp%ce%c or make a pr at https://github.com/MailTape/MailTape.github.io!", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: #22ACDD;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: black;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: #22ACDD;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: black;");
+        console.log("Hello Geeky Friend, want to help us improve %cM%cA%cI%cL%cT%cA%cP%cE%c experience? > contact@%cm%ca%ci%cl%ct%ca%c.%cp%ce%c or make a pr at https://github.com/MailTape/MailTape !", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: #22ACDD;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: black;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: #22ACDD;", "color: #FBE206;", "color: #ed1370;", "color: #23B14C;", "color: #F03C2D;", "color: black;");
     }
 );
