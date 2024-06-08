ec79ef17c7a39c296c5546d3624b7855b275d78e
ImaCrea <maximeguedj@pm.me>
2024-06-08 19:53:14 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import pagefind from "astro-pagefind";
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mailta.pe',
-	integrations: [mdx(), sitemap()],
+	trailingSlash: 'always',
+	integrations: [mdx(), sitemap(), pagefind()],
 });
