2ea65d68b59ec202bf4c18b7b542225ed49161c5
ImaCrea <maximeguedj@pm.me>
2024-06-08 22:20:17 +0200

    getting there

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mailta.pe',
-	trailingSlash: 'always',
 	integrations: [mdx(), sitemap(), pagefind()],
 });
