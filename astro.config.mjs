c1d8ebda6701d6293db65bf022bd334f944a7b7d
ImaCrea <maximeguedj@pm.me>
2024-12-16 12:16:22 +0100

    added Tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 import pagefind from "astro-pagefind";
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mailta.pe',
-	integrations: [mdx(), sitemap(), pagefind()],
+    site: 'https://mailta.pe',
+    integrations: [mdx(), sitemap(), pagefind(), tailwind(
+        {applyBaseStyles: false}
+    )],
 });
