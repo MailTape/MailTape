a857f70b30a5924fe55829a44ee29f36bae06d81
ImaCrea <maxime@mailta.pe>
2025-02-14 00:34:46 +0100

    nav fixed! (#9)

--- src/utils/slugEp.js
+++ src/utils/slugEp.js
@@ -1,7 +1,7 @@
 import slugify from "slugify"
 
 export default function slugEp(episode) {
-    return `${episode.data.category}`+"/"+slugify(`${episode.data.guest_name}`,{lower:true})
+    return `${episode?.data.category}`+"/"+slugify(`${episode?.data.guest_name}`,{lower:true})
   }
 
 // generate mailtape slug to keep URL consistent with old jekyll version (e.g mailta.pe/546/dear-nora)
