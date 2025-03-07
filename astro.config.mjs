fe4fd55850650974e25669be99ff35f7c3d09833
ImaCrea <maximeguedj@pm.me>
2025-03-07 16:04:33 +0100

    updated astrop and dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,15 @@ import sitemap from '@astrojs/sitemap';
 
 import pagefind from "astro-pagefind";
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mailta.pe',
-    integrations: [mdx(), sitemap(), pagefind(), tailwind(
-        {applyBaseStyles: false}
-    )],
+    integrations: [mdx(), sitemap(), pagefind()],
+    vite: {
+        plugins: [tailwindcss(
+            {applyBaseStyles: false}
+        )],
+    },
 });
